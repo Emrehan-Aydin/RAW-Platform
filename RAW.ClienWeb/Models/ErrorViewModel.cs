b0bbfcb544ff200cf42dc28494137211993e053f
Emrehan AYDIN <78824631+Emrehan-Aydin@users.noreply.github.com>
2023-01-29 18:33:15 +0300

    Remove Unused Project

--- RAW.ClienWeb/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace RAW.ClienWeb.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
